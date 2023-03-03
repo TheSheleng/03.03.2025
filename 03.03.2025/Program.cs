e7d8f6fe75f0985db8abf4f3e1a4dc7eb0866c0a
TheSheleng <sheleng55@gmail.com>
2023-03-03 15:13:34 +0200

    Add project files.

---
+++ 03.03.2025/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _03._03._2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            #region Task1
+            //Console.Write("Write string: ");
+            //input = Console.ReadLine();
+            //var matches = Regex.Matches(input, @"a[a-z]b", RegexOptions.IgnoreCase);
+            //foreach (var match in matches) { Console.Write($"{match}\t"); }
+            #endregion
+
+            #region Task2
+            //Console.Write("Write string: ");
+            //input = Console.ReadLine();
+            //var matches = Regex.Matches(input, @"a[a-z]{2}a", RegexOptions.IgnoreCase);
+            //foreach (var match in matches) { Console.Write($"{match}\t"); }
+            #endregion
+
+            #region Task3
+            //Console.Write("Write string: ");
+            //input = Console.ReadLine();
+            //var matches = Regex.Matches(input, @"ab[a-z]a", RegexOptions.IgnoreCase);
+            //foreach (var match in matches) { Console.Write($"{match}\t"); }
+            #endregion
+
+            #region Task4
+            //Console.Write("Write string: ");
+            //input = Console.ReadLine();
+            //var matches = Regex.Matches(input, @"ab+a", RegexOptions.IgnoreCase);
+            //foreach (var match in matches) { Console.Write($"{match}\t"); }
+            #endregion
+
+            #region Task5
+            //Console.Write("Write string: ");
+            //input = Console.ReadLine();
+            //var matches = Regex.Matches(input, @"ab*a", RegexOptions.IgnoreCase);
+            //foreach (var match in matches) { Console.Write($"{match}\t"); }
+            #endregion
+
+            #region Task6
+            //Console.Write("Write string: ");
+            //input = Console.ReadLine();
+            //var matches = Regex.Matches(input, @"ab?a", RegexOptions.IgnoreCase);
+            //foreach (var match in matches) { Console.Write($"{match}\t"); }
+            #endregion
+
+            #region Task7
+            //Console.Write("Write string: ");
+            //input = Console.ReadLine();
+            //var matches = Regex.Matches(input, @"ab*a", RegexOptions.IgnoreCase);
+            //foreach (var match in matches) { Console.Write($"{match}\t"); }
+            #endregion
+
+            #region Task8
+            //Console.Write("Write string: ");
+            //input = Console.ReadLine();
+            //var matches = Regex.Matches(input, @"ab?a", RegexOptions.IgnoreCase);
+            //foreach (var match in matches) { Console.Write($"{match}\t"); }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
